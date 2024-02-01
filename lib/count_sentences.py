16894433e9a490ae8b9ef7a06c809a594590581f
Kevin Kelley <kkelley622@gmail.com>
2024-02-01 17:04:54 -0600

    All tests are passing.

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=''):
+    self.value = value
+
+  def get_value(self):
+    return self._value
+  
+  def set_value(self, value):
+    if isinstance(value, str):
+      self._value = value
+
+    else:
+      print("The value must be a string.")
+
+  value = property(get_value, set_value)
+
+  def is_sentence(self):
+    return self.value.endswith('.')
+
+  def is_question(self):
+    return self.value.endswith('?')
+
+  def is_exclamation(self):
+    return self.value.endswith('!')
+  
+  def count_sentences(self):
+    no_exclamation = self.value.replace('!', '?')
+
+    all_periods = no_exclamation.replace('?', '.')
+
+    sentences = [s for s in all_periods.split('.') if s]
+
+    
+    return len(sentences)
